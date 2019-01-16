feebf00252fce835c1ca72c54f312d610996e234
Jamen <yhatotz19.ja@gmail.com>
2019-01-16 12:37:59 +0800

    Test

--- assets/plugins/codrops-stepsform/js/classie.js
+++ assets/plugins/codrops-stepsform/js/classie.js
@@ -1,7 +1,7 @@
 /*!
  * classie - class helper functions
  * from bonzo https://github.com/ded/bonzo
- *
+ * 
  * classie.has( elem, 'my-class' ) -> true/false
  * classie.add( elem, 'my-new-class' )
  * classie.remove( elem, 'my-unwanted-class' )
@@ -11,67 +11,70 @@
 /*jshint browser: true, strict: true, undef: true */
 /*global define: false */
 
-(function(window) {
-  // 'use strict';
+( function( window ) {
 
-  // class helper functions from bonzo https://github.com/ded/bonzo
+'use strict';
 
-  function classReg(className) {
-    return new RegExp('(^|\\s+)' + className + '(\\s+|$)');
-  }
+// class helper functions from bonzo https://github.com/ded/bonzo
 
-  // classList support for class management
-  // altho to be fair, the api sucks because it won't accept multiple classes at once
-  var hasClass, addClass, removeClass;
+function classReg( className ) {
+  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+}
 
-  if ('classList' in document.documentElement) {
-    hasClass = function(elem, c) {
-      return elem.classList.contains(c);
-    };
-    addClass = function(elem, c) {
-      elem.classList.add(c);
-    };
-    removeClass = function(elem, c) {
-      elem.classList.remove(c);
-    };
-  } else {
-    hasClass = function(elem, c) {
-      return classReg(c).test(elem.className);
-    };
-    addClass = function(elem, c) {
-      if (!hasClass(elem, c)) {
-        elem.className = elem.className + ' ' + c;
-      }
-    };
-    removeClass = function(elem, c) {
-      elem.className = elem.className.replace(classReg(c), ' ');
-    };
-  }
+// classList support for class management
+// altho to be fair, the api sucks because it won't accept multiple classes at once
+var hasClass, addClass, removeClass;
 
-  function toggleClass(elem, c) {
-    var fn = hasClass(elem, c) ? removeClass : addClass;
-    fn(elem, c);
-  }
-
-  var classie = {
-    // full names
-    hasClass: hasClass,
-    addClass: addClass,
-    removeClass: removeClass,
-    toggleClass: toggleClass,
-    // short names
-    has: hasClass,
-    add: addClass,
-    remove: removeClass,
-    toggle: toggleClass
+if ( 'classList' in document.documentElement ) {
+  hasClass = function( elem, c ) {
+    return elem.classList.contains( c );
   };
+  addClass = function( elem, c ) {
+    elem.classList.add( c );
+  };
+  removeClass = function( elem, c ) {
+    elem.classList.remove( c );
+  };
+}
+else {
+  hasClass = function( elem, c ) {
+    return classReg( c ).test( elem.className );
+  };
+  addClass = function( elem, c ) {
+    if ( !hasClass( elem, c ) ) {
+      elem.className = elem.className + ' ' + c;
+    }
+  };
+  removeClass = function( elem, c ) {
+    elem.className = elem.className.replace( classReg( c ), ' ' );
+  };
+}
 
-  // transport
-  if (typeof define === 'function' && define.amd) {
-    // AMD
-    define(classie);
-  } else {
-    // browser global
-    window.classie = classie;
-  }
-})(window);
+function toggleClass( elem, c ) {
+  var fn = hasClass( elem, c ) ? removeClass : addClass;
+  fn( elem, c );
+}
+
+var classie = {
+  // full names
+  hasClass: hasClass,
+  addClass: addClass,
+  removeClass: removeClass,
+  toggleClass: toggleClass,
+  // short names
+  has: hasClass,
+  add: addClass,
+  remove: removeClass,
+  toggle: toggleClass
+};
+
+// transport
+if ( typeof define === 'function' && define.amd ) {
+  // AMD
+  define( classie );
+} else {
+  // browser global
+  window.classie = classie;
+}
+
+})( window );
--- assets/plugins/codrops-stepsform/js/stepsForm.js
+++ assets/plugins/codrops-stepsform/js/stepsForm.js
@@ -4,236 +4,233 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- *
+ * 
  * Copyright 2014, Codrops
  * http://www.codrops.com
  */
-(function(window) {
-  // 'use strict';
+;( function( window ) {
+	
+	'use strict';
 
-  var transEndEventNames = {
-      WebkitTransition: 'webkitTransitionEnd',
-      MozTransition: 'transitionend',
-      OTransition: 'oTransitionEnd',
-      msTransition: 'MSTransitionEnd',
-      transition: 'transitionend'
-    },
-    transEndEventName = transEndEventNames[Modernizr.prefixed('transition')],
-    support = { transitions: Modernizr.csstransitions };
+	var transEndEventNames = {
+			'WebkitTransition': 'webkitTransitionEnd',
+			'MozTransition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'msTransition': 'MSTransitionEnd',
+			'transition': 'transitionend'
+		},
+		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+		support = { transitions : Modernizr.csstransitions };
 
-  function extend(a, b) {
-    for (var key in b) {
-      if (b.hasOwnProperty(key)) {
-        a[key] = b[key];
-      }
-    }
-    return a;
-  }
+	function extend( a, b ) {
+		for( var key in b ) { 
+			if( b.hasOwnProperty( key ) ) {
+				a[key] = b[key];
+			}
+		}
+		return a;
+	}
 
-  function stepsForm(el, options) {
-    this.el = el;
-    this.options = extend({}, this.options);
-    extend(this.options, options);
-    this._init();
-  }
+	function stepsForm( el, options ) {
+		this.el = el;
+		this.options = extend( {}, this.options );
+  		extend( this.options, options );
+  		this._init();
+	}
 
-  stepsForm.prototype.options = {
-    onSubmit: function() {
-      return false;
-    }
-  };
+	stepsForm.prototype.options = {
+		onSubmit : function() { return false; }
+	};
 
-  stepsForm.prototype._init = function() {
-    // current question
-    this.current = 0;
+	stepsForm.prototype._init = function() {
+		// current question
+		this.current = 0;
 
-    // questions
-    this.questions = [].slice.call(
-      this.el.querySelectorAll('ol.questions > li')
-    );
-    // total questions
-    this.questionsCount = this.questions.length;
-    // show first question
-    classie.addClass(this.questions[0], 'current');
+		// questions
+		this.questions = [].slice.call( this.el.querySelectorAll( 'ol.questions > li' ) );
+		// total questions
+		this.questionsCount = this.questions.length;
+		// show first question
+		classie.addClass( this.questions[0], 'current' );
+		
+		// next question control
+		this.ctrlNext = this.el.querySelector( 'button.next' );
 
-    // next question control
-    this.ctrlNext = this.el.querySelector('button.next');
+		// progress bar
+		this.progress = this.el.querySelector( 'div.progress' );
+		
+		// question number status
+		this.questionStatus = this.el.querySelector( 'span.number' );
+		// current question placeholder
+		this.currentNum = this.questionStatus.querySelector( 'span.number-current' );
+		this.currentNum.innerHTML = Number( this.current + 1 );
+		// total questions placeholder
+		this.totalQuestionNum = this.questionStatus.querySelector( 'span.number-total' );
+		this.totalQuestionNum.innerHTML = this.questionsCount;
 
-    // progress bar
-    this.progress = this.el.querySelector('div.progress');
+		// error message
+		this.error = this.el.querySelector( 'span.error-message' );
+		
+		// init events
+		this._initEvents();
+	};
 
-    // question number status
-    this.questionStatus = this.el.querySelector('span.number');
-    // current question placeholder
-    this.currentNum = this.questionStatus.querySelector('span.number-current');
-    this.currentNum.innerHTML = Number(this.current + 1);
-    // total questions placeholder
-    this.totalQuestionNum = this.questionStatus.querySelector(
-      'span.number-total'
-    );
-    this.totalQuestionNum.innerHTML = this.questionsCount;
+	stepsForm.prototype._initEvents = function() {
+		var self = this,
+			// first input
+			firstElInput = this.questions[ this.current ].querySelector( 'input' ),
+			// focus
+			onFocusStartFn = function() {
+				firstElInput.removeEventListener( 'focus', onFocusStartFn );
+				classie.addClass( self.ctrlNext, 'show' );
+			};
 
-    // error message
-    this.error = this.el.querySelector('span.error-message');
+		// show the next question control first time the input gets focused
+		firstElInput.addEventListener( 'focus', onFocusStartFn );
 
-    // init events
-    this._initEvents();
-  };
+		// show next question
+		this.ctrlNext.addEventListener( 'click', function( ev ) { 
+			ev.preventDefault();
+			self._nextQuestion(); 
+		} );
 
-  stepsForm.prototype._initEvents = function() {
-    var self = this,
-      // first input
-      firstElInput = this.questions[this.current].querySelector('input'),
-      // focus
-      onFocusStartFn = function() {
-        firstElInput.removeEventListener('focus', onFocusStartFn);
-        classie.addClass(self.ctrlNext, 'show');
-      };
+		// pressing enter will jump to next question
+		this.el.addEventListener( 'keydown', function( ev ) {
+			var keyCode = ev.keyCode || ev.which;
+			// enter
+			if( keyCode === 13 ) {
+				ev.preventDefault();
+				self._nextQuestion();
+			}
+		} );
 
-    // show the next question control first time the input gets focused
-    firstElInput.addEventListener('focus', onFocusStartFn);
+		// disable tab
+		this.el.addEventListener( 'keydown', function( ev ) {
+			var keyCode = ev.keyCode || ev.which;
+			// tab
+			if( keyCode === 9 ) {
+				ev.preventDefault();
+			} 
+		} );
+	};
 
-    // show next question
-    this.ctrlNext.addEventListener('click', function(ev) {
-      ev.preventDefault();
-      self._nextQuestion();
-    });
+	stepsForm.prototype._nextQuestion = function() {
+		if( !this._validade() ) {
+			return false;
+		}
 
-    // pressing enter will jump to next question
-    this.el.addEventListener('keydown', function(ev) {
-      var keyCode = ev.keyCode || ev.which;
-      // enter
-      if (keyCode === 13) {
-        ev.preventDefault();
-        self._nextQuestion();
-      }
-    });
+		// check if form is filled
+		if( this.current === this.questionsCount - 1 ) {
+			this.isFilled = true;
+		}
 
-    // disable tab
-    this.el.addEventListener('keydown', function(ev) {
-      var keyCode = ev.keyCode || ev.which;
-      // tab
-      if (keyCode === 9) {
-        ev.preventDefault();
-      }
-    });
-  };
+		// clear any previous error messages
+		this._clearError();
 
-  stepsForm.prototype._nextQuestion = function() {
-    if (!this._validade()) {
-      return false;
-    }
+		// current question
+		var currentQuestion = this.questions[ this.current ];
 
-    // check if form is filled
-    if (this.current === this.questionsCount - 1) {
-      this.isFilled = true;
-    }
+		// increment current question iterator
+		++this.current;
 
-    // clear any previous error messages
-    this._clearError();
+		// update progress bar
+		this._progress();
 
-    // current question
-    var currentQuestion = this.questions[this.current];
+		if( !this.isFilled ) {
+			// change the current question number/status
+			this._updateQuestionNumber();
 
-    // increment current question iterator
-    ++this.current;
+			// add class "show-next" to form element (start animations)
+			classie.addClass( this.el, 'show-next' );
 
-    // update progress bar
-    this._progress();
+			// remove class "current" from current question and add it to the next one
+			// current question
+			var nextQuestion = this.questions[ this.current ];
+			classie.removeClass( currentQuestion, 'current' );
+			classie.addClass( nextQuestion, 'current' );
+		}
 
-    if (!this.isFilled) {
-      // change the current question number/status
-      this._updateQuestionNumber();
+		// after animation ends, remove class "show-next" from form element and change current question placeholder
+		var self = this,
+			onEndTransitionFn = function( ev ) {
+				if( support.transitions ) {
+					this.removeEventListener( transEndEventName, onEndTransitionFn );
+				}
+				if( self.isFilled ) {
+					self._submit();
+				}
+				else {
+					classie.removeClass( self.el, 'show-next' );
+					self.currentNum.innerHTML = self.nextQuestionNum.innerHTML;
+					self.questionStatus.removeChild( self.nextQuestionNum );
+					// force the focus on the next input
+					nextQuestion.querySelector( 'input' ).focus();
+				}
+			};
 
-      // add class "show-next" to form element (start animations)
-      classie.addClass(this.el, 'show-next');
+		if( support.transitions ) {
+			this.progress.addEventListener( transEndEventName, onEndTransitionFn );
+		}
+		else {
+			onEndTransitionFn();
+		}
+	}
 
-      // remove class "current" from current question and add it to the next one
-      // current question
-      var nextQuestion = this.questions[this.current];
-      classie.removeClass(currentQuestion, 'current');
-      classie.addClass(nextQuestion, 'current');
-    }
+	// updates the progress bar by setting its width
+	stepsForm.prototype._progress = function() {
+		this.progress.style.width = this.current * ( 100 / this.questionsCount ) + '%';
+	}
 
-    // after animation ends, remove class "show-next" from form element and change current question placeholder
-    var self = this,
-      onEndTransitionFn = function(ev) {
-        if (support.transitions) {
-          this.removeEventListener(transEndEventName, onEndTransitionFn);
-        }
-        if (self.isFilled) {
-          self._submit();
-        } else {
-          classie.removeClass(self.el, 'show-next');
-          self.currentNum.innerHTML = self.nextQuestionNum.innerHTML;
-          self.questionStatus.removeChild(self.nextQuestionNum);
-          // force the focus on the next input
-          nextQuestion.querySelector('input').focus();
-        }
-      };
+	// changes the current question number
+	stepsForm.prototype._updateQuestionNumber = function() {
+		// first, create next question number placeholder
+		this.nextQuestionNum = document.createElement( 'span' );
+		this.nextQuestionNum.className = 'number-next';
+		this.nextQuestionNum.innerHTML = Number( this.current + 1 );
+		// insert it in the DOM
+		this.questionStatus.appendChild( this.nextQuestionNum );
+	}
 
-    if (support.transitions) {
-      this.progress.addEventListener(transEndEventName, onEndTransitionFn);
-    } else {
-      onEndTransitionFn();
-    }
-  };
+	// submits the form
+	stepsForm.prototype._submit = function() {
+		this.options.onSubmit( this.el );
+	}
 
-  // updates the progress bar by setting its width
-  stepsForm.prototype._progress = function() {
-    this.progress.style.width =
-      this.current * (100 / this.questionsCount) + '%';
-  };
+	// TODO (next version..)
+	// the validation function
+	stepsForm.prototype._validade = function() {
+		// current question´s input
+		var input = this.questions[ this.current ].querySelector( 'input' ).value;
+		if( input === '' ) {
+			this._showError( 'EMPTYSTR' );
+			return false;
+		}
 
-  // changes the current question number
-  stepsForm.prototype._updateQuestionNumber = function() {
-    // first, create next question number placeholder
-    this.nextQuestionNum = document.createElement('span');
-    this.nextQuestionNum.className = 'number-next';
-    this.nextQuestionNum.innerHTML = Number(this.current + 1);
-    // insert it in the DOM
-    this.questionStatus.appendChild(this.nextQuestionNum);
-  };
+		return true;
+	}
 
-  // submits the form
-  stepsForm.prototype._submit = function() {
-    this.options.onSubmit(this.el);
-  };
+	// TODO (next version..)
+	stepsForm.prototype._showError = function( err ) {
+		var message = '';
+		switch( err ) {
+			case 'EMPTYSTR' : 
+				message = 'Please fill the field before continuing';
+				break;
+			case 'INVALIDEMAIL' : 
+				message = 'Please fill a valid email address';
+				break;
+			// ...
+		};
+		this.error.innerHTML = message;
+		classie.addClass( this.error, 'show' );
+	}
 
-  // TODO (next version..)
-  // the validation function
-  stepsForm.prototype._validade = function() {
-    // current question´s input
-    var input = this.questions[this.current].querySelector('input').value;
-    if (input === '') {
-      this._showError('EMPTYSTR');
-      return false;
-    }
+	// clears/hides the current error message
+	stepsForm.prototype._clearError = function() {
+		classie.removeClass( this.error, 'show' );
+	}
 
-    return true;
-  };
+	// add to global namespace
+	window.stepsForm = stepsForm;
 
-  // TODO (next version..)
-  stepsForm.prototype._showError = function(err) {
-    var message = '';
-    switch (err) {
-      case 'EMPTYSTR':
-        message = 'Please fill the field before continuing';
-        break;
-      case 'INVALIDEMAIL':
-        message = 'Please fill a valid email address';
-        break;
-      // ...
-    }
-    this.error.innerHTML = message;
-    classie.addClass(this.error, 'show');
-  };
-
-  // clears/hides the current error message
-  stepsForm.prototype._clearError = function() {
-    classie.removeClass(this.error, 'show');
-  };
-
-  // add to global namespace
-  window.stepsForm = stepsForm;
-})(window);
+})( window );
